c0dcb7e2a7f367111c317fa81a10785c42dda3dc
Bjelland14 <lloydbjelland@gmail.com>
2025-10-29 15:37:52 +0100

    Update index.html

--- js/api.js
+++ js/api.js
@@ -1,9 +1,15 @@
-export const BASE= "https://v2.api.noroff.dev";
-const shop = `$ {BASE}/online-shop`;
+export const BASE = "https://v2.api.noroff.dev";
+const SHOP = `${BASE}/online-shop`;
 
 export async function getProducts({ limit = 12, page = 1 } = {}) {
-    const qs = new URLSearchParams ({  limit, page }). toString();
-    const res = await fetch (`$ {SHOP}?${qs}`);   
-    if (!res.ok) throw new Error ("Failed to fetch products");
-    return res.json();
+  const qs = new URLSearchParams({ limit, page }).toString();
+  const res = await fetch(`${SHOP}?${qs}`);
+  if (!res.ok) throw new Error("Failed to fetch products");
+  return res.json();
+}
+
+export async function getProduct(id) {
+  const res = await fetch(`${SHOP}/${id}`);
+  if (!res.ok) throw new Error("Failed to fetch product");
+  return res.json();
 }
--- js/home.js
+++ js/home.js
@@ -1,33 +1,34 @@
-import {getProducts} from "./api.js";
+import { getProducts } from "./api.js";
 
-const grid= document.getElementById ("products-grid");
-const statusEl = document.getElementById ("status");
+const grid = document.getElementById("products-grid");
+const statusEl = document.getElementById("status");
 const carousel = document.getElementById("carousel");
 
-(async function () => {
-    try{
-    const{ data: items} =await getProducts ({ limit: 12)}
-    grid.innerHTML = items.map (card).join("");
+(async () => {
+  try {
+    const { data: items } = await getProducts({ limit: 12 });
+    grid.innerHTML = items.map(card).join("");
 
-    const { data: featured } = await getProducts ({ limit: 3  });
+    const { data: featured } = await getProducts({ limit: 3 });
+    carousel.innerHTML = featured.map(card).join("");
 
-    statusEl.textContent=""
-    } catch (err){
-        statusEl.textContent=err.message || "Something went wrong.";
-        }
+    statusEl.textContent = "";
+  } catch (err) {
+    statusEl.textContent = err.message || "Something went wrong.";
+  }
 })();
 
-function card (p){
-    const price=
+function card(p) {
+  const price =
     p.discountedPrice && p.discountedPrice < p.price
-    ? `<s>${p.price.toFixed(2)} NOK</s> <strong> ${p.discountedPrice.toFixed(2)} NOK </strong>`
-    : <strong>$ (p.price.toFixed(2)} NOK </strong>;
+      ? `<s>${p.price.toFixed(2)} NOK</s> <strong>${p.discountedPrice.toFixed(2)} NOK</strong>`
+      : `<strong>${p.price.toFixed(2)} NOK</strong>`;
 
-    return`
-    <a a class="card" href="/product.html?id=${p.id}">
-    <img src "${p.image?.url}" alt="${p.image?.alt || p.title}">
-    <h3>${p.title}</h3>
-    <p>${price}</p>
+  return `
+    <a class="card" href="./product.html?id=${p.id}">
+      <img src="${p.image?.url}" alt="${p.image?.alt || p.title}">
+      <h3>${p.title}</h3>
+      <p>${price}</p>
     </a>
-    `;
+  `;
 }
